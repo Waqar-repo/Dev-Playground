6b0e9d9af87e2cf97488b9af72a023298c83c10d
Waqar-repo <vizzahmad@gmail.com>
2025-06-04 22:33:11 +0100

    1

---
+++ Javascript/03-code-execution-js/script.js
@@ -0,0 +1,10 @@
+// debugger
+console.log(firstName)
+var city = 'pirmahal'
+var firstName = 'Waqar';
+let lastName = 'Ahamd';
+let age = 15;
+const yearOfBirth = 1999;
+let userIntro = 'Hi My name is ' + firstName + ' '  + lastName ;
+
+//JS put let and const temporal dead zone TDZ untill initializes it with a value 
