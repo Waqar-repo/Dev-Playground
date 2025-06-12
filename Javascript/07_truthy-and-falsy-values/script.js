301c1c1c24131010ec43005d8a6d1e87432fdc2a
Waqar-repo <vizzahmad@gmail.com>
2025-06-12 17:22:01 +0100

    1.0
    understading Falsy Values

---
+++ Javascript/07_truthy-and-falsy-values/script.js
@@ -0,0 +1,15 @@
+console.log('-0 is ' + Boolean(-0))
+console.log('0 is ' + Boolean(0))
+console.log('NaN is ' + Boolean(NaN))
+
+console.log(` Empty string '' is '  ${Boolean('')}`)
+console.log(` undefined is '  ${Boolean(undefined)}`)
+console.log('null is ' + Boolean(null))
+
+console.log('!-0 is ' + Boolean(!-0))
+console.log('!0 is ' + Boolean(!0))
+console.log('!NaN is ' + Boolean(!NaN))
+
+console.log(` !Empty string '' is '  ${Boolean(!'')}`)
+console.log(` !undefined is '  ${Boolean(!undefined)}`)
+console.log('1 null is ' + Boolean(!null))
