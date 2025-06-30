42b92ed57a12c33b94a51328ad39cca42e828e1b
Waqar-repo <vizzahmad@gmail.com>
2025-06-30 15:20:41 +0100

    1.0

---
+++ Javascript/15-Visualize-Variables-Address/script.js
@@ -0,0 +1,9 @@
+
+const firstName = 'Waqar'
+const firstName2 = 'Waqar'
+const lastName = 'Ahmad'
+const lastName2 = 'Ah'+'mad'
+const userAge = 15
+const birthYear  = 2000
+const isGraduate = false
+const hasJob = true
