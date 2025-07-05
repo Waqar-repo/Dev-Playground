954463d59c107b8c418c533767eea6072f5506b9
Waqar-repo <vizzahmad@gmail.com>
2025-07-05 19:42:56 +0100

    1.0

---
+++ Javascript/22-compound-operators/script.js
@@ -0,0 +1,112 @@
+
+
+let num = 5
+
+num = num + 5
+
+//+= Addition Assignment Operator.
+
+num += 5
+console.log(num);
+
+console.log('---------------------');
+console.log('-= Subtraction Assignment Operator.');
+numSubstract = 5
+numSubstract -= 3
+console.log(numSubstract);
+
+console.log('---------------------');
+
+console.log('%= Remainder Assignment Operator.');
+
+numRemainder = 5
+
+numRemainder %= 3
+
+console.log(numRemainder);
+
+console.log('---------------------');
+
+console.log('*= Multiplication Assignment Operator.');
+
+numMultiplication = 5
+
+numMultiplication *= 5
+console.log(numMultiplication);
+
+
+console.log('---------------------');
+
+console.log('/= Division Assignment Operator');
+
+numDivision = 6
+
+numDivision /= 3
+
+console.log(numDivision);
+
+console.log('---------------------');
+
+console.log('**= Exponentiation Assignment Operator.');
+
+numExponentiation = 5
+
+numExponentiation **= 5
+console.log(numExponentiation);
+
+
+console.log('---------------------');
+//Incremen + operator increase a number with one 
+console.log('Increment ++');
+
+let numIncrement = 5
+
+//numIncrement++ will return then increment the value
+numIncrement++
+numIncrement++
+//++numIncrement will increment the vlaue then return 
+++numIncrement
+++numIncrement
+
+
+console.log(numIncrement);
+
+
+
+
+console.log('---------------------');
+
+//Decrement -- decrease a number with one 
+console.log('Decrement --');
+
+let numDecrement = 10
+--numDecrement 
+--numDecrement
+
+console.log(numDecrement);
+
+let x = 5
+
+//afte this the value of  x would be 6
+//but a = 5 
+let a = x++
+
+
+let x1 = 5
+//in this case value of x1 and b would be 6
+let b = ++x1
+
+
+
+console.log('---------------------');
+
+let xi = 5
+
+//afte this the value of  x would be 4
+//but a = 5 
+let c = xi--
+
+
+let xii = 5
+//in this case value of x1 and b would be 4
+let d = --xii
---
+++ Javascript/22-compound-operators/scripts2.js
@@ -0,0 +1,26 @@
+
+const name = 'waqar123'
+
+let num = 5
+
+let b = num++
+console.log('Post increment');
+
+console.log('let num = 5');
+console.log('let b = num++');
+console.log(`b = ` + b);
+
+console.log(`num = ` + num);
+
+
+console.log('---------------------');
+
+let num1 = 5
+
+let b1 = ++num1
+console.log('Pre increment');
+console.log('let num1 = 5');
+console.log('let b1 = ++num1  ');
+console.log(`b1 = ` + b1);
+
+console.log(`num1 = ` + num1);
