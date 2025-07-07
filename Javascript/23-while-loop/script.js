d3b9158e2dd0577781e567175f23c367c09f0d8f
Waqar-repo <vizzahmad@gmail.com>
2025-07-08 00:01:26 +0100

    1.0

---
+++ Javascript/23-while-loop/script.js
@@ -0,0 +1,62 @@
+console.log('Program Started');
+
+// let num = 1;
+
+// debugger
+
+// while(num <= 100){
+
+//     console.log(num);
+
+// num++
+
+// }
+
+// console.log('Program Ended');
+
+console.log('------');
+//task
+console.log('Task');
+
+// const friendName = ['Waqar', 'Asad', 'Raza', 'Rana', 'Humza',];
+
+// let i = 0
+// while(i < friendName.length){
+//     let lastname = 'Procoder'
+
+//     console.log(`${i+1}. ${friendName[i]} ${lastname}`);
+//     i++
+// }
+
+
+// let i = 1
+// while(i <= 20){
+//     if(i % 2 !== 0){
+//     console.log(i);
+//     }
+//     i++
+// }
+
+
+
+// const question = prompt('What is your Password').toUpperCase()
+// const passWord = 'ALPINE'
+
+// while(question !== passWord){
+//     question = prompt('Wrong password try again').toUpperCase()
+
+    
+// }
+// alert(`Correct Password`)
+
+
+let num = 1
+let sum = 0
+while(num <= 50){
+  sum += num
+
+ num++
+
+    
+}
+ console.log(sum);
