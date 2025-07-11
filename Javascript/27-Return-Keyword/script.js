fbfc2c9adf189acde103c21042f1ff724b5ede7b
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-11 15:17:29 +0100

    startup code

---
+++ Javascript/27-Return-Keyword/script.js
@@ -0,0 +1,15 @@
+
+// Function Definition
+function introduceMe(username, profession, age) {
+
+    console.log('Hi,');
+    console.log(`My name is ${username || 'Code Geek'}.`);
+    console.log(`I am a ${profession}`);
+    console.log(`I am ${age} years old.`); 
+}
+
+// //                  Function Call
+// const returnValue = introduceMe()
+
+// introduceMe('Waqar Ahmad', 'Software Engineer', 25)
+// introduceMe('Zeeshan', 'Mechanical Engineer', 27)
