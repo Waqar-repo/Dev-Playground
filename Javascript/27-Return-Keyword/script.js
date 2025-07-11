23a46981ab3faadc549bd3d4ccb0df464adf4061
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-11 15:59:29 +0100

    understand the return value of a function

--- Javascript/27-Return-Keyword/script.js
+++ Javascript/27-Return-Keyword/script.js
@@ -1,15 +1,50 @@
 
-// Function Definition
-function introduceMe(username, profession, age) {
+//parameter pass to calculate sum
+function addTwoNumber(num1, num2){
 
-    console.log('Hi,');
-    console.log(`My name is ${username || 'Code Geek'}.`);
-    console.log(`I am a ${profession}`);
-    console.log(`I am ${age} years old.`); 
+//return will return to function caller 
+return num1 + num2
+}
+// debugger
+
+//fucntion calling
+const result = addTwoNumber(22,12)
+console.log(result);
+
+const test = addTwoNumber(addTwoNumber(5,5),addTwoNumber(20,20))
+console.log(test);
+
+console.log('---------');
+console.log('substract two number function');
+
+//parameters two substract two number
+function substractTwoNumber(a,b){
+
+    //return to function caller/calling
+    return a - b
+
+}
+const subResult = substractTwoNumber(6,4)
+console.log(subResult);
+
+console.log('---------');
+console.log('multiply two number function');
+
+function multiTwoNumber(a,b){
+    return a * b
 }
 
-// //                  Function Call
-// const returnValue = introduceMe()
+const multiResult = multiTwoNumber(2,2)
 
-// introduceMe('Waqar Ahmad', 'Software Engineer', 25)
-// introduceMe('Zeeshan', 'Mechanical Engineer', 27)
+console.log(multiResult);
+
+console.log('---------');
+console.log('multiply three number function');
+
+function multiThreeNumber(a,b,c){
+    return a * b * c
+}
+// debugger
+const multiResult3 = multiThreeNumber(2,2,4)
+
+console.log(multiResult3);
