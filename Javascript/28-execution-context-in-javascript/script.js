a5fb2156fb5ecf9245b0023872185139edceb757
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-12 13:32:20 +0100

    understand function execution context

---
+++ Javascript/28-execution-context-in-javascript/script.js
@@ -0,0 +1,18 @@
+debugger
+sayHi()
+// console.log(firstName)
+
+// var firstName = 'Waqar'
+// let lastName = 'Ahmad'
+// let age = 15
+// const yearOfBirth = 1999
+
+var userName = 'Waqar Ahmad'
+var userAge = 34
+
+function sayHi(){
+    const a = 2
+    const b = 4
+    console.log(a,b);
+}
+
