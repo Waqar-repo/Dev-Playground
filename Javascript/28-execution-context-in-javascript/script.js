73c9dd7c548e6de27012dfc29d9b8ad6a54979d9
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-12 14:29:00 +0100

    understand the execution context in nasted fucntion call

--- Javascript/28-execution-context-in-javascript/script.js
+++ Javascript/28-execution-context-in-javascript/script.js
@@ -7,12 +7,27 @@ sayHi()
 // let age = 15
 // const yearOfBirth = 1999
 
-var userName = 'Waqar Ahmad'
-var userAge = 34
+const userName = 'Waqar Ahmad'
+const userAge = 34
 
 function sayHi(){
     const a = 2
     const b = 4
+    add(2,2)
     console.log(a,b);
+    console.log('say local context');
 }
 
+function add(x,y){
+    subs(6,3)
+    return x + y
+    
+    
+
+}
+
+function subs(w,a){
+    return w - a
+}
+
+console.log('program ended');
