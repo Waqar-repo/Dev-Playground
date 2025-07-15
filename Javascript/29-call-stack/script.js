d87037648ef2944f3d28807f6141eece73fe1086
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-15 18:37:39 +0100

    starter code

---
+++ Javascript/29-call-stack/script.js
@@ -0,0 +1,23 @@
+debugger
+
+sayHi()
+
+const username = 'Waqar'
+const userAge = 34
+
+function sayHi() {
+    const a = 14
+    const b = 12
+    add(7, 9)
+}
+
+function add(x, y) {
+    kuchhBhi()
+    return x + y
+}
+
+function kuchhBhi() {
+    console.log('Kuchh bhi');
+}
+
+console.log('Program Ended');
