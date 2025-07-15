cac3c5d32ce79630c9c823dfc463545be140a395
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-15 19:07:19 +0100

    understand call stack and overflow

--- Javascript/29-call-stack/script.js
+++ Javascript/29-call-stack/script.js
@@ -1,23 +1,32 @@
-debugger
+//A call stack is a mechanism for an interpreter 
+// (like the JavaScript interpreter in a web browser) 
+// to keep track of its place in a script that calls 
+// multiple functions
+debugger;
 
-sayHi()
+sayHi();
 
-const username = 'Waqar'
-const userAge = 34
+const username = 'Waqar';
+const userAge = 34;
 
 function sayHi() {
-    const a = 14
-    const b = 12
-    add(7, 9)
+  const a = 14;
+  const b = 12;
+  add(7, 9);
 }
 
 function add(x, y) {
-    kuchhBhi()
-    return x + y
+  kuchhBhi();
+  return x + y;
 }
 
 function kuchhBhi() {
-    console.log('Kuchh bhi');
+  console.log('Kuchh bhi');
 }
 
-console.log('Program Ended');
+// function introduceMe(){
+//     console.log('Hi, My name is Waqar Ahmad');
+
+// }
+// introduceMe()
+// console.log('Program Ended');
