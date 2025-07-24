595ffb364deba1c741a826cc8e0a568c62aff533
Waqar Ahmad <vizzahmad@gmail.com>
2025-07-24 17:27:41 +0100

    understand settimeout and setinterval

--- Javascript/34-setTimeout-and-setInterval/script.js
+++ Javascript/34-setTimeout-and-setInterval/script.js
@@ -1,5 +1,5 @@
 //inside setTImeout() is single quote it will take it as veriable or java script code not string or numer etc
-// it also returns the value which isnthe id of the time
+// it also returns the value which is the id of the time
 
 
 //this can execute the code user desired time in milliseconds
@@ -12,7 +12,7 @@
 
 // debugger
 const time1 = setTimeout(`console.log("Hi")`,1000)
-const time2 = setTimeout(`console.log("Hi2")`,2000)
+const time2 = setTimeout(`console.log("Hi2")`)
 // setTimeout(a(),4000) will explicitly call function will print the value and return undefined 
 // and time3 will not run as it will have setTimeout(undefined,4000)
 // const time3 = setTimeout(a(),4000)
@@ -27,6 +27,30 @@ const time3 = setTimeout(a,4000)
 // console.log(time3);
 
 // practical use
+
+//passing named fucntion
+const time4 = setTimeout(a,3000,'assd',99,'asdasd',[1,7])
+
+//passing Ananymous function
+setTimeout(function(){
+    console.log('Hi,Ananymous');
+},1000)
 function a(){
-console.log('Hello World');
+// console.log(arguments);
+    console.log('Hello World');
+
 }
+//question which one will console.log 1st
+
+//ans: console.log('adsasdasasdsadsad') will print first then
+//then const time2 = setTimeout(`console.log("Hi2")`)
+//java script normal code is synchronous(line by line)
+//but settimeout() asynchronous function (does not happen right away)
+console.log('adsasdasasdsadsad');
+
+
+//setinterval() will run the code on repeate according to given time
+
+// const time5 = setInterval(`console.log("Hi-interval")`,1000)
+// const time6 = setInterval(`console.log("Hi2-interval")`)
+const time7 = setInterval(a,4000)
