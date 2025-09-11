d63dd3caad736ec41bf5b37b8c11f3583542befb
Waqar Ahmad <vizzahmad@gmail.com>
2025-09-11 12:24:19 +0100

    understand next/previous sibling

--- Javascript/54-accessing-parent-sibling-and-children-elements/script.js
+++ Javascript/54-accessing-parent-sibling-and-children-elements/script.js
@@ -36,3 +36,102 @@ for (const link of allLinks) {
     link.classList.remove('my-link')
     // link.classList.toggle('my-link')
 }
+
+
+// new lesson
+//all the elements will be a node but there are other things are node apart from elements
+// to know/access the parent of an element, we save that element into veriable
+const firstlink = document.querySelector("body > p:nth-child(5) > a.hii.hello.green-link")
+
+//then nfirstlink.parentElement will give us the parent of the element, in this case ptag
+firstlink.parentElement
+
+//if we run parent element on previous parent element , we get its parent, which is body in thsi case
+firstlink.parentElement.parentElement
+
+//here it will give us html in this case
+firstlink.parentElement.parentElement.parentElement
+
+//not html does has any parent so return null
+firstlink.parentElement.parentElement.parentElement.parentElement
+
+//but parent node of html is document
+firstlink.parentElement.parentElement.parentElement.parentNode
+
+//to access the children of an element
+
+//in this case, it does has any child
+firstlink.children
+
+//but here we have body right now
+firstlink.parentElement.parentElement
+
+//so we accessing parent and then accessing it child,
+//now all children of body will be shown in thsi case
+
+firstlink.parentElement.parentElement.children
+//output of above code
+//firstlink.parentElement.parentElement.children
+// HTMLCollection(9) [h1.css-image, h2#hii.heading-1, h2, hr, p, img, ul, p, script, hii: h2#hii.heading-1]
+// 0
+// : 
+// h1.css-image
+// 1
+// : 
+// h2#hii.heading-1
+// 2
+// : 
+// h2
+// 3
+// : 
+// hr
+// 4
+// : 
+// p
+// 5
+// : 
+// img
+// 6
+// : 
+// ul
+// 7
+// : 
+// p
+// 8
+// : 
+// script
+// hii
+// : 
+// h2#hii.heading-1
+// length
+// : 
+// 9
+
+//access siblings  ie next sibling and previous sibling
+
+// nextSibling returns the next node (an element node, a text node or a comment node).
+//  Whitespace between elements are also text nodes.
+// nextElementSibling returns the next element (not text and comment nodes).
+
+
+//previousSibling returns the previous node (an element node, a text node or a comment node).
+//  Whitespace between elements are also text nodes.
+// previousElementSibling returns the previous element (not text and comment nodes).
+//you can not access 3rd sibling next or previous, we nede to access step by step
+
+
+//tis will give us next sibling on the element we have selected, html ITC
+firstlink.nextElementSibling
+
+//this will give us next sibling of next sibling, css in this case
+firstlink.nextElementSibling.nextElementSibling
+
+//java script in thsi case
+firstlink.nextElementSibling.nextElementSibling.nextElementSibling
+
+// this will return null in this case, as it parent (ptag) ends.
+//siblings can be under one parent 
+firstlink.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling
+
+//same rules for previous sibling
+firstlink.previousSibling
