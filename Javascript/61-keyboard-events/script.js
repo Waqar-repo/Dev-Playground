fa61b493043ab5ebf2217dcec08f135a4ace47df
Waqar Ahmad <vizzahmad@gmail.com>
2025-09-20 14:04:49 +0100

    understand keyboard event

---
+++ Javascript/61-keyboard-events/script.js
@@ -0,0 +1,53 @@
+//there are three kind of keyboard event
+//keyup 
+//keydown
+//keypress
+//we can apply keyboard event on any element,but it should be on focus
+
+const h1 = document.querySelector('h1')
+const input = document.querySelector('input')
+//keypress
+//if element is not focusable like h1, to focus it,
+//  we have to give tabindex (tabindex="0">)
+
+//usually we usually do that on windows
+//keypress count key keyup and key down
+// window.addEventListener('keypress', (e) => {
+    
+//     console.log('Key code :',e.code);
+//     console.log('value :',e.key);
+//     console.log(e.charCode);
+  
+// })
+
+// h1.addEventListener('keypress', (e) => {
+//     console.log(e.key);
+// })
+
+// input.addEventListener('keypress', (e) => {
+//     console.log(e.key);
+// })
+
+
+//keyup only fired when key leave the key 
+// it will also fire if we press shift,alt f5 etc unlike keypress
+// window.addEventListener('keyup', (e) => {
+//     console.log(e);
+//    console.log('Key code :',e.code);
+//     console.log('value :',e.key);
+//     console.log(e.charCode);
+  
+// })
+
+
+
+//keydown
+//it only fires when we press any key
+// it will also fire if we press shift,alt f5 etc unlike keypress
+window.addEventListener('keydown', (e) => {
+    console.log(e);
+   console.log('Key code :',e.code);
+    console.log('value :',e.key);
+    console.log(e.charCode);
+  
+})
