f9d3892be23c80cdb4e5ede09b2fbc7acba29724
Waqar Ahmad <vizzahmad@gmail.com>
2025-09-21 22:05:24 +0100

    practice and challenges

--- Javascript/61-keyboard-events/script.js
+++ Javascript/61-keyboard-events/script.js
@@ -56,12 +56,14 @@ window.addEventListener('keydown', (e) => {
 
 //practice
 
-// Mini Coding Challenges
-// Challenge 1: Detect Enter key to submit
+/* 
+Mini Coding Challenges
+Challenge 1: Detect Enter key to submit
 
-//  When the user types inside an input and presses Enter, display the input value inside a <p> tag below it.
-// const inputBox = document.querySelector('input');
-// const output = document.querySelector('#output');
+ When the user types inside an input and presses Enter, display the input value inside a <p> tag below it.
+const inputBox = document.querySelector('input');
+const output = document.querySelector('#output');
+*/
 
 // inputBox.addEventListener('keydown', (event) => {
 //   let newarr = ''
@@ -76,6 +78,22 @@ window.addEventListener('keydown', (e) => {
 
 // Challenge 2: Keyboard Shortcuts
 
-//  If the user presses Ctrl + S, show an alert saying "Save triggered!".
-// If the user presses Ctrl + D, show "Download triggered!".
+ /* If the user presses Ctrl + S, show an alert saying "Save triggered!".
+If the user presses Ctrl + D, show "Download triggered!". */
 
+
+// const inputBox = document.querySelector('input');
+// const output = document.querySelector('#output');
+
+// window.addEventListener('keydown', (event) => {
+//    if(event.ctrlKey && event.key.toLowerCase()=== 's')
+//    {
+//     event.preventDefault();
+//    console.log('Save triggered!')
+//    }
+//    else if (event.ctrlKey && event.key.toLowerCase() === 'd')
+//    {
+//     event.preventDefault();
+// console.log('download triggered!')
+//    }
+// });
