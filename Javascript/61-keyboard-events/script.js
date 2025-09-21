93d1891fef0ad1210e63ed3d2eac0b1d2cb4a278
Waqar Ahmad <vizzahmad@gmail.com>
2025-09-21 11:42:40 +0100

    practice

--- Javascript/61-keyboard-events/script.js
+++ Javascript/61-keyboard-events/script.js
@@ -51,3 +51,31 @@ window.addEventListener('keydown', (e) => {
     console.log(e.charCode);
   
 })
+
+
+
+//practice
+
+// Mini Coding Challenges
+// Challenge 1: Detect Enter key to submit
+
+//  When the user types inside an input and presses Enter, display the input value inside a <p> tag below it.
+// const inputBox = document.querySelector('input');
+// const output = document.querySelector('#output');
+
+// inputBox.addEventListener('keydown', (event) => {
+//   let newarr = ''
+// if(event.code == 'Enter'){
+//    newarr += inputBox.value
+
+// output.innerHTML = newarr
+// }
+
+// });
+
+
+// Challenge 2: Keyboard Shortcuts
+
+//  If the user presses Ctrl + S, show an alert saying "Save triggered!".
+// If the user presses Ctrl + D, show "Download triggered!".
+
