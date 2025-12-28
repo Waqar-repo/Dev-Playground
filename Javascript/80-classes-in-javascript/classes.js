6d7c9a9ba4d95b95ec1ce3b84bc8d16f6c10e490
Waqar Ahmad <vizzahmadgmail.com>
2025-12-28 18:40:32 +0000

    practice and play with UI API AND CLASSES

--- Javascript/80-classes-in-javascript/classes.js
+++ Javascript/80-classes-in-javascript/classes.js
@@ -39,3 +39,49 @@ const user2 = new CreateUser('Sahil', 'Ahmad', 72)
 //     return this.firstName + ' ' + this.lastName
 // }
 
+/* Practice */
+const ul = document.querySelector('ul')
+
+class Product {
+    constructor(brand,des,price){
+this.brand = brand;
+this.des = des;
+this.price = price;
+    }
+}
+
+async function getProcts() {
+    const url = 'https://dummyjson.com/products/1'
+    const res = await fetch(url)
+    const data = await res.json()
+    // console.log(res);
+    // console.log(data);
+
+    const product1 = new Product(data.brand,data.description,data.price)
+    console.log(product1);
+    console.log(typeof product1);
+
+    // const ul1 = document.createElement('li')
+    // ul1.innerText = data.brand
+    // ul2.innerText = data.description
+    // ul3.innerText = data.price
+
+    // ul.appendChild(ul1)
+    // ul.appendChild(ul2)
+    // ul.appendChild(ul3)
+const len = Object.values(product1).length
+for(var l in product1){
+    console.log(l , product1[l])
+}
+
+// Object.values(product1).forEach(value =>{
+//     const li = document.createElement('li')
+//     li.innerText = value
+//     ul.appendChild(li)
+// })
+for (let key in product1){
+    const li = document.createElement('li')
+    li.innerText = product1[key]
+    ul.appendChild(li)
+}
+}
