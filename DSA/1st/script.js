bb82a1e66996c0f3c08c2a1ac9101203ea092a8b
Waqar Ahmad <vizzahmadgmail.com>
2026-01-10 14:24:11 +0000

    1

---
+++ DSA/1st/script.js
@@ -0,0 +1,152 @@
+
+/*
+
+Print Numbers from 1 to N
+
+Input: 5
+Output: 1 2 3 4 5
+✨ Teaches basic loop construction.
+
+*/
+
+// let input = 5
+// let output = 0
+
+// for ( let i = 1; i <= input;i++){
+//     output++
+//     console.log(output);
+// }
+
+console.log('------2--------------');
+
+
+/*
+Print Numbers from N to 1 without changing the loop condition of above question
+
+Input: 5
+Output: 5 4 3 2 1
+✨ Thinking creatively and manipulating logic within the loop
+
+*/
+
+// while(input > 0){
+//     output = input--
+//     console.log(output);
+    
+// }
+
+
+console.log('------3--------------');
+
+
+/*
+
+Print All Even Numbers from 1 to N
+
+Input: 10
+Output: 2 4 6 8 10
+✨ Introduces conditional checks inside loops.
+
+*/
+
+
+// let input = 10
+// let output = 0
+
+// for( let i = 1; i <= input; i++){
+    
+//     if(i % 2 === 0){
+// console.log(i);
+
+//     }
+// }
+
+console.log('------4--------------');
+
+/**
+ Sum of First N Natural Numbers
+
+Input: 5
+Output: 15
+✨ Encourages cumulative addition and variable usage.
+ */
+
+// const n = 5
+// let output = 0
+// for(i = 1; i <= n; i++){
+
+//     output = output + i
+    
+// }
+// console.log(output);
+
+
+console.log('------5--------------');
+
+
+/*
+
+Product (Factorial) of N
+
+Input: 5
+Output: 120
+✨ Reinforces loop control with multiplication logic.
+
+*/
+
+// const n = 5
+// let output = 1
+// for(i = 1; i <= n; i++){
+// console.log(i);
+//     console.log('fac',output = output * i);
+    
+    
+// }
+// console.log(output);
+
+
+console.log('------6--------------');
+
+/*
+
+Sum of All Even Numbers up to N
+
+Input: 10
+Output: 30 (2+4+6+8+10)
+✨ Combines loops + condition + accumulation.
+
+*/
+
+// const num = 10
+// let output = 0
+
+// for (let i=1;i<=num;i++){
+//     if(i % 2 === 0){
+//         output+=i
+//     }
+// }
+// console.log(output);
+
+
+console.log('------7--------------');
+
+/*
+
+Print Squares of Numbers from 1 to N
+
+Input: 5
+Output: 1 4 9 16 25
+✨ Introduces arithmetic pattern generation.
+
+
+*/
+
+// const num = 5
+
+// for(let i = 1; i<=num;i++){
+//     console.log(i*i);
+// }
+
+
+//Practice
+
