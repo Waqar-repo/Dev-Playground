1dfb273c7110633db17ced962446d85d5a0e2b6b
Waqar Ahmad <vizzahmadgmail.com>
2026-01-11 18:07:55 +0000

    practice

--- DSA/1st/script.js
+++ DSA/1st/script.js
@@ -116,18 +116,28 @@ Output: 30 (2+4+6+8+10)
 ✨ Combines loops + condition + accumulation.
 
 */
+console.time()
+const num = 1000000000
+let output = 0
 
-// const num = 10
-// let output = 0
+for (let i=1;i<=num;i++){
+    if(i % 2 === 0){
+        output+=i
+    }
+}
+console.log(output);
+console.timeEnd()
 
-// for (let i=1;i<=num;i++){
-//     if(i % 2 === 0){
-//         output+=i
-//     }
-// }
-// console.log(output);
+console.time()
 
+function sumEvenNUm(n){
+    const largeEvenNum = n % 2 === 0 ? n:n-1
+    const sum = (largeEvenNum *(largeEvenNum + 2)/4)
+    return sum
+}
+console.log(sumEvenNUm(1000000));
 
+console.timeEnd()
 console.log('------7--------------');
 
 /*
@@ -165,9 +175,9 @@ Output: 15 30
 
 */
 
-// const num = 30
+// const num33= 30
 
-// for(let i = 1; i <= num;i++){
+// for(let i = 1; i <= num33;i++){
   
 //     if(i % 3 === 0 && i % 5 === 0){
 // console.log(i);
@@ -256,31 +266,26 @@ Output: 4 16
 
 */
 
-const input = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-  31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-  41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-  51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-  71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-  81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-  91, 92, 93, 94, 95, 96, 97, 98, 99, 100
-]
+// const input = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//   11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+//   21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+//   31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+//   41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+//   51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+//   61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+//   71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+//   81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+//   91, 92, 93, 94, 95, 96, 97, 98, 99, 100
+// ]
 
-let output=[]
-for(let i = 1; i<= input.length;i++){
+// let output=[]
+// for(let i = 1; i<= input.length;i++){
     
-    let sqrt = Math.sqrt(i)
-    if(sqrt % 1===0 && sqrt % 2 ===0){
-        // console.log(i);
-        output.push(i)
-    }
-}
-console.log(output);
-
-
-
-
-
+//     let sqrt = Math.sqrt(i)
+//     if(sqrt % 1===0 && sqrt % 2 ===0){
+//         // console.log(i);
+//         output.push(i)
+//     }
+// }
+// console.log(output)
