eda54024b79cbe5fa3031e01165a35930f2cb312
Waqar Ahmad <vizzahmad@gmail.com>
2026-01-21 21:28:09 +0000

    understand what is JSX and Babel, how it works

---
+++ ReactJs/jsx-and-babel/artical.md
@@ -0,0 +1,15 @@
+JavaScript XML (JSX) and Babel
+
+So, I am working in React js which is a famous JavaScript library which help to create element/component easily than vanilla java script. We can create web element/components with the help of React using react.createElement(‘div’) and render it. But it is kind of long process which can be shortened using JSX. JSX is a easy way to write html tags in java script but react/JavaScript do not understand it. So we need to find a way that JSX convert into React create element syntax so react can run the code and render it on webpage. 
+To solve this problem we have babel, babel is a next gen compiler which can convert modern JavaScript into a version that an old browser or environment can understand. For that we can either use stand-alone script or install it using npm. 
+JSX saves developer’s time and minimise the complexity or creating element in react . 
+But there are some key points to remember while writing JSX.
+
+
+(i)--to run a java script code inside JSX we have to use curly brackets {} and put code/variable etc inside the bracket. 
+
+(i)--Write CSS/html  in JSX, we have to understand the syntax and variable names that a java script use. For example, inside html label tag normally use ‘for’ attributes but actual it is htmlFor and ‘class’ is className. So, keep that in mind and use correct variable names.
+
+
+
+
--- ReactJs/jsx-and-babel/output/script.js
+++ ReactJs/jsx-and-babel/output/script.js
@@ -35,52 +35,48 @@
 // babel is javascript compiler 
 // convert jsx into react elements using babel.
 
-const container = (
-  <div className="container" id="container">
-    <section className="sectionOne">
-      <p>React is beautiful</p>
-      <img
-        className="img"
-        src="http://tiny.cc/uyux001"
-        style={{
-            width: 250,
-            borderRadius: 8,
-            backgroundColor: 'yellow',
-            padding: 16,
-              }}
-      />
-    </section>
-    <section className="sectionTwo">
-      <form className="form">
-        <div className="inputGroup">
-          <label htmlFor="userName">User Name </label>
-          <input id="userName" />
-        </div>
-        <div className="inputGroup">
-          <label htmlFor="Password"> Password </label>
-          <input id="Password" type="password" />
-        </div>
-      </form>
-    </section>
-  </div>
-);
-
-
-
+var container = /*#__PURE__*/React.createElement("div", {
+  className: "container",
+  id: "container"
+}, /*#__PURE__*/React.createElement("section", {
+  className: "sectionOne"
+}, /*#__PURE__*/React.createElement("p", null, "React is beautiful"), /*#__PURE__*/React.createElement("img", {
+  className: "img",
+  src: "http://tiny.cc/uyux001",
+  style: {
+    width: 250,
+    borderRadius: 8,
+    backgroundColor: 'yellow',
+    padding: 16
+  }
+})), /*#__PURE__*/React.createElement("section", {
+  className: "sectionTwo"
+}, /*#__PURE__*/React.createElement("form", {
+  className: "form"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "inputGroup"
+}, /*#__PURE__*/React.createElement("label", {
+  htmlFor: "userName"
+}, "User Name "), /*#__PURE__*/React.createElement("input", {
+  id: "userName"
+})), /*#__PURE__*/React.createElement("div", {
+  className: "inputGroup"
+}, /*#__PURE__*/React.createElement("label", {
+  htmlFor: "Password"
+}, " Password "), /*#__PURE__*/React.createElement("input", {
+  id: "Password",
+  type: "password"
+})))));
 
 // const h2 = <h2>Hello JSX</h2>
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+var root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(container);
-
-
-
-const user = 'waqar'
+var user = 'waqar';
 //JS code are pass into curley brackets in html tags{user}
-const h2 = <h2>hello <b>{user}</b></h2>  
+var h2 = /*#__PURE__*/React.createElement("h2", null, "hello ", /*#__PURE__*/React.createElement("b", null, user));
 
 // root.render(h2);
 // console.log(h2);
-
 
 // const h2 = document.createElement('h2')
 // document.querySelector('#root').append(h2)
