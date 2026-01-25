77a6f2dc7774d9dd044eab65bc83348335eb74aa
Waqar Ahmad <vizzahmadgmail.com>
2026-01-25 12:54:34 +0000

    practice how to use react element and fetch api render

--- ReactJs/05-rendering-list-of-element/script.js
+++ ReactJs/05-rendering-list-of-element/script.js
@@ -1,41 +1,94 @@
 import { createRoot } from 'react-dom/client';
 import './style.css';
-const h1 = <h1>Hello World</h1>;
+// const h1 = <h1>Hello World</h1>;
 
-function Card(key, title, img, brand, price) {
-  return (
-    <div className="card" key={key}>
-      <img src={img} alt="iphone" />
-      <div className="card-content">
-        <h3>{title}</h3>
-        <p>{brand}</p>
-        <p>
-          $ <b>{price}</b>
-        </p>
-      </div>
-    </div>
-  );
+// function Card(key, title, img, brand, price) {
+//   return (
+//     <div className="card" key={key}>
+//       <img src={img} alt="iphone" />
+//       <div className="card-content">
+//         <h3>{title}</h3>
+//         <p>{brand}</p>
+//         <p>
+//           $ <b>{price}</b>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+// // const container = [Card(1),Card(2),Card(3),Card(4),Card(5)]
+
+// const root = createRoot(document.getElementById('root'));
+
+// // console.log(Card());
+
+// fetch('https://dummyjson.com/products')
+//   .then((res) => res.json())
+//   .then((data) => {
+//     root.render(
+//       <div className="container">
+//         {data.products.map((product) => {
+//           return Card(
+//             product.id,
+//             product.title,
+//             product.thumbnail,
+//             product.brand,
+//             product.price,
+//           );
+//         })}
+//       </div>,
+//     );
+//   });
+
+
+//practice
+
+// id,img ,firstname last name ,role email
+
+
+
+
+
+function app(key,first,last,img,gender,role,email) {
+  return <div className='card' key={key}>
+  
+<img src={img} alt="picture" />
+<div className="card-content">
+<p><b>{first} {last}</b></p>
+<p>Gender: {gender}</p>
+<p>Role:   {role}</p>
+<p> {email}</p>
+</div>
+
+</div>
+  
 }
-// const container = [Card(1),Card(2),Card(3),Card(4),Card(5)]
 
-const root = createRoot(document.getElementById('root'));
+// const temp = [ app(1),app(2),app(3),app(4) ]
+const root = createRoot(document.getElementById('root'))
+// root.render(<div className='container'>{temp}</div>)
 
-// console.log(Card());
+fetch('https://dummyjson.com/users')
+.then(res => res.json())
+.then( (data) => {
+  console.log(data.users);
+  root.render(<div className='container'>
+    {
+      data.users.map((user)=>{
+        return app(
+          user.id,
+          user.firstName,
+          user.lastName,
+          user.image,
+          user.gender,
+          user.role,
+          user.email
 
-fetch('https://dummyjson.com/products')
-  .then((res) => res.json())
-  .then((data) => {
-    root.render(
-      <div className="container">
-        {data.products.map((product) => {
-          return Card(
-            product.id,
-            product.title,
-            product.thumbnail,
-            product.brand,
-            product.price,
-          );
-        })}
-      </div>,
-    );
-  });
+
+        )
+      })
+    }
+  
+  </div>
+  )
+})
