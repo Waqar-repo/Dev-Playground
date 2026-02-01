3ce02f55481556874838020ae22c07d0ec27adf6
Waqar Ahmad <vizzahmad@gmail.com>
2026-02-01 15:24:33 +0000

    delete files

--- ReactJs/10-states-in-react-vite/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ imageUrl, titleText, altText, onClickHandler }) => {
-  return (
-    <button title={titleText} onClick={onClickHandler}>
-      <img src={imageUrl} alt={altText} />
-    </button>
-  );
-};
-
-export default Button;
